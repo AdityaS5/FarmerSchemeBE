94760c5957dcb84b60c64e1227e2d8a6b0fbe173
AdityaS5 <aditya.sable511@gmail.com>
2021-04-15 16:28:40 +0530

    admin controller

---
+++ FarmerScheme/Controllers/AdminController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FarmerScheme.Models;
+
+namespace FarmerScheme.Controllers
+{
+    public class AdminController : ApiController
+    {
+        FarmerSchemeEntities db = new FarmerSchemeEntities();
+
+        public IHttpActionResult GetAdmin()
+        {
+            return Ok(db.admins.ToList());
+        }
+        
+        //[HttpPost]
+        //public IHttpActionResult Register(admin admin)
+        //{
+        //    db.admins.Add(admin);
+        //    db.SaveChanges();
+        //    return Ok(db.admins);
+        //}
+
+        //public IHttpActionResult PutUser(string email_id, admin admin)
+        //{
+        //    db.Entry(admin).State = EntityState.Modified;
+        //    db.SaveChanges();
+        //    return Ok(admin);
+        //}
+
+        //public IHttpActionResult DeleteUser(string email_id)
+        //{
+        //    admin admin = db.admins.Find(email_id);
+        //    db.admins.Remove(admin);
+        //    db.SaveChanges();
+        //    return Ok(admin);
+        //}
+
+
+    }
+}
---
+++ FarmerScheme/Controllers/AdminUserApprovalController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FarmerScheme.Models;
+namespace FarmerScheme.Controllers
+{
+    public class AdminUserApprovalController : ApiController
+    {
+        FarmerSchemeEntities db = new FarmerSchemeEntities();
+        public IHttpActionResult Getuser_information()
+        {
+            return Ok(db.user_information.ToList());
+        }
+    }
+}
